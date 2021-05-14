219fa8cadc73b5dcabbc6e73b031bb8b329dec55
Andrew Welch <andrew@nystudio107.com>
2021-05-13 20:51:00 -0400

    Make the cache last for 30 seconds with devMode on
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- src/services/ViteService.php
+++ src/services/ViteService.php
@@ -42,7 +42,7 @@ class ViteService extends Component
     const CACHE_KEY = 'vite';
     const CACHE_TAG = 'vite';
 
-    const DEVMODE_CACHE_DURATION = 1;
+    const DEVMODE_CACHE_DURATION = 30;
 
     const USER_AGENT_STRING = 'User-Agent:Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13';
 
